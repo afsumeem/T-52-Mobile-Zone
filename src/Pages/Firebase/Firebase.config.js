510f672018fd180e5737bc06c66dba494bc30378
kawsaralidev <mdkawsarali346@gmail.com>
2022-01-02 11:58:25 +0600

    loginsystem

---
+++ src/Pages/Firebase/Firebase.config.js
@@ -0,0 +1,19 @@
+// const firebaseConfig = {
+//     apiKey: process.env.REACT_APP_API_KEY,
+//     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+//     projectId: process.env.REACT_APP_PROJECT_ID,
+//     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+//     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+//     appId: process.env.REACT_APP_APP_ID,
+
+// };
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCkbKu1Ndva5JpP6TdYwA9WtG9zHblO0gM",
+  authDomain: "t-52-mobile-zone.firebaseapp.com",
+  projectId: "t-52-mobile-zone",
+  storageBucket: "t-52-mobile-zone.appspot.com",
+  messagingSenderId: "390285679428",
+  appId: "1:390285679428:web:9efd721abc0e6d9445d511",
+};
+export default firebaseConfig;
--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Button } from "react-bootstrap";
+import "./Banner.css";
 
 const Banner = () => {
   return (
@@ -7,7 +8,7 @@ const Banner = () => {
       <div className="container">
         <div className="row py-5 ">
           <div className=" col-lg-6 col-sm-12 text-start ">
-            <h2 className="fs-1 fw-bold text-info  ">
+            <h2 className="fs-1 fw-bold text-style  ">
               Phone Lover
               <br /> For Every Phones
             </h2>
@@ -15,7 +16,7 @@ const Banner = () => {
               A mobile phone is one kind of portable telephone. A great discovery of science. We get and send news, play game, time,
               calculator, etc. everything from the phone.
             </p>
-            <Button className="bg-primary p-2 rounded-4 mt-4 fs-5 fw-bold"> Buy Now</Button>
+            <Button className="buynow-btn mt-4 fs-5 "> Buy Now</Button>
           </div>
           <div className="col-lg-6 col-sm-12 ">
             <img className="w-100" src="https://i.ibb.co/846J3CM/phone2.webp" alt="" />
