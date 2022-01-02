92c1de3522de4c1440420923f8f15e05d7c63c57
foridislam332 <foridislam332>
2022-01-02 11:17:26 +0600

    Latest Products added

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Banner from "../Banner/Banner";
 import BestNewItems from "../BestNewItems/BestNewItems";
+import LatestProducts from "../LatestProducts/LatestProducts";
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
             <BestNewItems></BestNewItems>
+            <LatestProducts></LatestProducts>
         </div>
     );
 };
---
+++ src/Pages/Home/LatestProducts/LatestProducts.js
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from 'react';
+import { Container } from 'react-bootstrap';
+import LatestProduct from '../LatestProduct/LatestProduct';
+
+const LatestProducts = () => {
+    const [products, setProducts] = useState([]);
+    const [newProducts, setNewProducts] = useState([]);
+    useEffect(() => {
+        fetch('./fakeData.json')
+            .then(res => res.json())
+            .then(data => setProducts(data))
+    }, [])
+
+    const condition = "New";
+
+    useEffect(() => {
+        if (products.length) {
+            const findNewProducts = products.filter(product => product.condition == condition);
+            setNewProducts(findNewProducts)
+        }
+    }, [products])
+
+    return (
+        <div className="products_container">
+            <Container>
+                <div className="section_title">
+                    <h2>Latest Products ...</h2>
+                    <p>Updated with the latest products</p>
+                </div>
+                <div className="latest_product_box">
+                    <div className="row gy-4">
+                        {
+                            newProducts.slice(0, 4).map(product => <LatestProduct
+                                key={product.id}
+                                product={product}
+                            ></LatestProduct>)
+                        }
+                    </div>
+                </div>
+            </Container>
+        </div>
+    );
+};
+
+export default LatestProducts;
