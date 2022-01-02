adbe71300cb22eaef4b9173fe62fe9f1859d20cc
foridislam332 <foridislam332>
2022-01-02 21:20:55 +0600

    margin remove

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -8,7 +8,7 @@ import './Footer.css';
 
 const Footer = () => {
     return (
-        <div className='footer-container text-start mt-5'>
+        <div className='footer-container text-start'>
             <Container>
                 <Row className='pt-4 pb-2 gx-4 gy-3 m-0'>
                     <Col md={3} sm={6} xs={12}>
