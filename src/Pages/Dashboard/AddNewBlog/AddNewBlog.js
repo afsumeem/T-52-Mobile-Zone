fb5aea16423fb9ece6336350f942763e626c9d0e
foridislam332 <foridislam332>
2022-01-03 22:12:21 +0600

    blog post update

--- src/Pages/Dashboard/AddNewBlog/AddNewBlog.js
+++ src/Pages/Dashboard/AddNewBlog/AddNewBlog.js
@@ -1,17 +1,15 @@
 import axios from 'axios';
 import React from 'react';
-import { Button, Container } from 'react-bootstrap';
+import { Button, Col, Container, Row } from 'react-bootstrap';
 import { useForm } from 'react-hook-form';
 import useAuth from '../../Hooks/useAuth';
 
 const AddNewBlog = () => {
-
     const { user } = useAuth();
     const { register, handleSubmit, reset, formState: { errors } } = useForm();
 
     // handle submit
     const onSubmit = data => {
-
         axios.post('https://safe-coast-68587.herokuapp.com/blog', data)
             .then(res => {
                 if (res.data.insertedId) {
@@ -22,27 +20,42 @@ const AddNewBlog = () => {
     };
 
     return (
-        <Container>
-            <div className='section_title pt-3'>
-                <h2>Add New Blog</h2>
+        <Container className="my-5 pt-4">
+            <div className="section_title">
+                <h2>Add New Blogs ...</h2>
+                <p>Write your blogs</p>
             </div>
-            <form onSubmit={handleSubmit(onSubmit)} className='m-auto'>
+            <Row>
+                <Col md={1}></Col>
+                <Col md={10}>
+                    <div className="login_form">
+                        <div className="login_title mb-5">
+                            <h3>Write Your New Blogs</h3>
+                        </div>
+                        <form onSubmit={handleSubmit(onSubmit)} className='m-auto'>
+                            <label htmlFor="blog">Blog Title <span>*</span></label>
+                            <input placeholder='Enter your blog title' id="blog" {...register("name", { required: true })} />
 
-                <input className='d-block my-3 w-75 m-auto p-1' placeholder='Paste your image link' {...register("img", { required: true })} />
+                            <label htmlFor="blog_details">Blog Description <span>*</span></label>
+                            <textarea style={{ height: '100px', border: '1px solid #00000021', marginBottom: '12px' }} className="w-100 pt-2 ps-2" placeholder="Enter your blog description" id="blog_details" {...register("description", { required: true })} />
 
-                <input className='d-block my-3 w-75 m-auto p-1' placeholder='Blog title' {...register("name", { required: true })} />
+                            <label htmlFor="img_link">Image Link <span>*</span></label>
+                            <input placeholder='Paste your image link' id="img_link" {...register("img", { required: true })} />
 
-                <input className='d-block my-3 w-75 m-auto p-1' placeholder="Blog Details" {...register("description", { required: true })} />
+                            <label htmlFor="writer">Blog Writer <span>*</span></label>
+                            <input id="writer" placeholder='Enter writer name' {...register("author", { required: true })} />
 
-                <input className='d-block my-3 w-75 m-auto p-1' defaultValue={"by admin"} {...register("author", { required: true })} />
+                            <label htmlFor="writer">Blog Post Date <span>*</span></label>
+                            <input defaultValue={new Date().toISOString().substring(0, 10)} {...register("date", { required: true })} />
 
-                <input className='d-block my-3 w-75 m-auto p-1' defaultValue={new Date().toISOString().substring(0, 10)} {...register("date", { required: true })} />
-
-                <Button type="submit" className='quick_view d-block my-3 w-75 m-auto p-1'>
-                    Add Blog
-                </Button>
-
-            </form>
+                            <Button type="submit" className="btn_regular w-100 py-3 fw-bold">
+                                Post Your Blogs
+                            </Button>
+                        </form>
+                    </div>
+                </Col>
+                <Col md={1}></Col>
+            </Row>
         </Container>
     );
 };
