556d6c83198a53fc9184b06caac13287c8c4ec09
kawsaralidev <mdkawsarali346@gmail.com>
2022-01-03 12:20:34 +0600

    blog added

---
+++ src/Pages/Blogs/Blogs/Blogs.js
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from "react";
+import Blog from "../Blog/Blog";
+
+const Blogs = () => {
+  const [bloges, setBloges] = useState([]);
+
+  useEffect(() => {
+    fetch("./blogsData.json")
+      .then((res) => res.json())
+      .then((data) => setBloges(data));
+  }, []);
+  return (
+    <div className="container my-5 pt-4">
+      <div className="section_title ">
+        <h2>Our Blogs ...</h2>
+        <p>Updated with the our blogs</p>
+      </div>
+      <div className="d-flex row">
+        <div className="col-lg-4 border me-3">
+          <h3 className="py-5">
+            Recent <span style={{ color: "red" }}>Article</span>
+          </h3>
+
+          {bloges.map((bloge) => (
+            <div>
+              <div className="d-flex  ">
+                <div className="col-lg-5">
+                  <img className="w-100" src={bloge.img} alt="" />
+                </div>
+                <div className="col-lg-7 text-start  mt-4">
+                  <h4 className=" ">{bloge.name}</h4>
+                  <p className=""> June 2021</p>
+                  <h5 className="text-info"></h5>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+
+        <div className="row col-lg-8">
+          {bloges.map((blog) => (
+            <Blog key={blog.id} blog={blog}></Blog>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Blogs;
--- src/Pages/Shared/Navigation/Navigation.js
+++ src/Pages/Shared/Navigation/Navigation.js
@@ -15,128 +15,134 @@ const userIcon = <FontAwesomeIcon icon={faUserCircle} />;
 const faBarsIcon = <FontAwesomeIcon icon={faBars} />;
 
 const Navigation = () => {
-    const { user, logOut } = useFirebase();
-    return (
-        <div>
-            {/* top navbar */}
-            <Navbar collapseOnSelect expand="lg" style={{ background: "#ebebeb" }}>
-                <Container>
-                    <Navbar.Brand className="fw-bold" href="#home">
-                        <span style={{ color: "red" }}>T-52</span> Mobile Zone
-                    </Navbar.Brand>
-                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                    <Navbar.Collapse id="responsive-navbar-nav">
-                        <Nav className="mx-auto">
-                            <Form className="d-flex">
-                                <FormControl
-                                    type="search"
-                                    placeholder="Search"
-                                    aria-label="Search"
-                                    style={{ borderRadius: "30px 0 0 30px", border: "1px solid #17d74a !important" }}
-                                    className="search_input"
-                                />
-                                <Button className="search_btn" style={{ borderRadius: "0 30px 30px 0" }} variant="outline-success">
-                                    Search
-                                </Button>
-                            </Form>
-                        </Nav>
-                        <Nav>
-                            <Nav.Link eventKey={2} href="#memes">
-                                My Account
-                            </Nav.Link>
-                            <Nav.Link eventKey={2} href="#memes">
-                                Cart
-                            </Nav.Link>
-                            <Nav.Link eventKey={2} href="#memes">
-                                Checkout
-                            </Nav.Link>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
+  const { user, logOut } = useFirebase();
+  return (
+    <div>
+      {/* top navbar */}
+      <Navbar collapseOnSelect expand="lg" style={{ background: "#ebebeb" }}>
+        <Container>
+          <Navbar.Brand className="fw-bold" href="#home">
+            <span style={{ color: "red" }}>T-52</span> Mobile Zone
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mx-auto">
+              <Form className="d-flex">
+                <FormControl
+                  type="search"
+                  placeholder="Search"
+                  aria-label="Search"
+                  style={{ borderRadius: "30px 0 0 30px", border: "1px solid #17d74a !important" }}
+                  className="search_input"
+                />
+                <Button className="search_btn" style={{ borderRadius: "0 30px 30px 0" }} variant="outline-success">
+                  Search
+                </Button>
+              </Form>
+            </Nav>
+            <Nav>
+              <Nav.Link eventKey={2} href="#memes">
+                My Account
+              </Nav.Link>
+              <Nav.Link eventKey={2} href="#memes">
+                Cart
+              </Nav.Link>
+              <Nav.Link eventKey={2} href="#memes">
+                Checkout
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
 
-            {/* main navbar */}
-            <Navbar collapseOnSelect expand="lg" style={{ background: "#fef5ef", padding: "20px 0", boxShadow: '0 10px 40px 0px rgb(0 0 0 / 15%)' }}>
-                <Container>
-                    <NavDropdown title="All-Brands" id="collasible-nav-dropdown">
-                        <NavDropdown.Item as={Link} to="/apple">
-                            Apple
-                        </NavDropdown.Item>
-                        <NavDropdown.Item as={Link} to="/samsung">
-                            Samsung
-                        </NavDropdown.Item>
-                        <NavDropdown.Item as={Link} to="/huawei">
-                            Huawei
-                        </NavDropdown.Item>
-                        <NavDropdown.Item as={Link} to="/oppo">
-                            Oppo
-                        </NavDropdown.Item>
-                        <NavDropdown.Item as={Link} to="/vivo">
-                            Vivo
-                        </NavDropdown.Item>
-                        <NavDropdown.Item as={Link} to="/xiaomi">
-                            Xiaomi
-                        </NavDropdown.Item>
-                        <NavDropdown.Item as={Link} to="/realme">
-                            Realme
-                        </NavDropdown.Item>
-                    </NavDropdown>
-                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                    <Navbar.Collapse id="responsive-navbar-nav">
-                        <Nav className="mx-auto main_nav">
-                            <Nav.Link as={Link} to="/home">
-                                Home
-                            </Nav.Link>
-                            <Nav.Link as={Link} to="/shop">
-                                Shop
-                            </Nav.Link>
-                            <Nav.Link as={Link} to="/category">
-                                Category
-                            </Nav.Link>
-                            <Nav.Link as={Link} to="/blog">
-                                Blogs
-                            </Nav.Link>
-                            <Nav.Link as={Link} to="/about">
-                                About
-                            </Nav.Link>
-                        </Nav>
-                        <Nav>
-                            <Nav.Link className="total_price">USD: $00.00</Nav.Link>
-                            <Nav.Link className="nav_icon" href="#deets">
-                                {shoppingCart}
-                                <Badge>0</Badge>
-                            </Nav.Link>
-                            <Nav.Link className="nav_icon" href="#deets">
-                                {hardIcon}
-                                <Badge>0</Badge>
-                            </Nav.Link>
+      {/* main navbar */}
+      <Navbar
+        collapseOnSelect
+        expand="lg"
+        style={{ background: "#fef5ef", padding: "20px 0", boxShadow: "0 10px 40px 0px rgb(0 0 0 / 15%)" }}
+      >
+        <Container>
+          <NavDropdown title="All-Brands" id="collasible-nav-dropdown">
+            <NavDropdown.Item as={Link} to="/apple">
+              Apple
+            </NavDropdown.Item>
+            <NavDropdown.Item as={Link} to="/samsung">
+              Samsung
+            </NavDropdown.Item>
+            <NavDropdown.Item as={Link} to="/huawei">
+              Huawei
+            </NavDropdown.Item>
+            <NavDropdown.Item as={Link} to="/oppo">
+              Oppo
+            </NavDropdown.Item>
+            <NavDropdown.Item as={Link} to="/vivo">
+              Vivo
+            </NavDropdown.Item>
+            <NavDropdown.Item as={Link} to="/xiaomi">
+              Xiaomi
+            </NavDropdown.Item>
+            <NavDropdown.Item as={Link} to="/realme">
+              Realme
+            </NavDropdown.Item>
+          </NavDropdown>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mx-auto main_nav">
+              <Nav.Link as={Link} to="/home">
+                Home
+              </Nav.Link>
+              <Nav.Link as={Link} to="/shop">
+                Shop
+              </Nav.Link>
+              <Nav.Link as={Link} to="/category">
+                Category
+              </Nav.Link>
+              <Nav.Link as={Link} to="/blogs">
+                Blogs
+              </Nav.Link>
+              <Nav.Link as={Link} to="/about">
+                About
+              </Nav.Link>
+            </Nav>
+            <Nav>
+              <Nav.Link className="total_price">USD: $00.00</Nav.Link>
+              <Nav.Link className="nav_icon" href="#deets">
+                {shoppingCart}
+                <Badge>0</Badge>
+              </Nav.Link>
+              <Nav.Link className="nav_icon" href="#deets">
+                {hardIcon}
+                <Badge>0</Badge>
+              </Nav.Link>
 
-                            {/* user */}
-                            <div id="user_box">
-                                <NavDropdown title={userIcon} id="collasible-nav-dropdown">
-                                    <NavDropdown.Item>
-                                        {user.displayName}
-                                    </NavDropdown.Item>
-                                    <NavDropdown.Item as={Link} to="/account">
-                                        My Account
-                                    </NavDropdown.Item>
-                                    <NavDropdown.Item>
-                                        {user.email ? (<Button onClick={logOut} className="btn_regular">Log Out</Button>) : (
-                                            <Link to="/login">
-                                                <Button className="btn_regular" color="inherit">
-                                                    Login
-                                                </Button>
-                                            </Link>
-                                        )}
-                                    </NavDropdown.Item>
-                                </NavDropdown>
-                            </div>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-        </div>
-    );
+              {/* user */}
+              <div id="user_box">
+                <NavDropdown title={userIcon} id="collasible-nav-dropdown">
+                  <NavDropdown.Item>{user.displayName}</NavDropdown.Item>
+                  <NavDropdown.Item as={Link} to="/account">
+                    My Account
+                  </NavDropdown.Item>
+                  <NavDropdown.Item>
+                    {user.email ? (
+                      <Button onClick={logOut} className="btn_regular">
+                        Log Out
+                      </Button>
+                    ) : (
+                      <Link to="/login">
+                        <Button className="btn_regular" color="inherit">
+                          Login
+                        </Button>
+                      </Link>
+                    )}
+                  </NavDropdown.Item>
+                </NavDropdown>
+              </div>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
 };
 
 export default Navigation;
