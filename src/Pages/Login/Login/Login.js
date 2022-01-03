156d6d1490f2acb95558948336bbfda727312247
foridislam332 <foridislam332>
2022-01-03 20:18:49 +0600

    login form update

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,65 +1,73 @@
 import React, { useState } from "react";
-import { Button } from "react-bootstrap";
+import { Button, Col, Row } from "react-bootstrap";
 import { Link, useLocation, useNavigate } from "react-router-dom";
+import './Login.css';
 import useAuth from "../../Hooks/useAuth";
 
 const Login = () => {
-  const [loginData, setLoginData] = useState({});
-  const { loginUser, signInWithGoogle } = useAuth();
+    const [loginData, setLoginData] = useState({});
+    const { loginUser, signInWithGoogle } = useAuth();
 
-  const location = useLocation();
-  const navigate = useNavigate();
+    const location = useLocation();
+    const navigate = useNavigate();
 
-  const handleOnChange = (e) => {
-    const field = e.target.name;
-    const value = e.target.value;
-    const newLoginData = { ...loginData };
-    newLoginData[field] = value;
-    setLoginData(newLoginData);
-  };
+    const handleOnChange = (e) => {
+        const field = e.target.name;
+        const value = e.target.value;
+        const newLoginData = { ...loginData };
+        newLoginData[field] = value;
+        setLoginData(newLoginData);
+    };
 
-  const handleLoginSubmit = (e) => {
-    loginUser(loginData.email, loginData.password, location, navigate);
+    const handleLoginSubmit = (e) => {
+        loginUser(loginData.email, loginData.password, location, navigate);
 
-    e.preventDefault();
-  };
+        e.preventDefault();
+    };
 
-  const handleGoogleSignIn = () => {
-    signInWithGoogle(location, navigate);
-  };
+    const handleGoogleSignIn = () => {
+        signInWithGoogle(location, navigate);
+    };
 
 
-  return (
-    <div className="container  ">
-      <div className="row ">
-        <div className="col-lg-6 ">
-          <img className="w-100" src="https://image.freepik.com/free-vector/sign-concept-illustration_114360-5425.jpg" alt="" />
+    return (
+        <div className="container  ">
+            <Row>
+                <Col md={7}>
+                    <div className="form_img">
+                        <img className="w-100" src="https://image.freepik.com/free-vector/sign-concept-illustration_114360-5425.jpg" alt="" />
+                    </div>
+                </Col>
+                <Col md={5} className="d-flex align-items-center justify-content-center">
+                    <div className="login_form">
+                        <div className="login_title mb-4">
+                            <h3>Please Login</h3>
+                        </div>
+                        <form onSubmit={handleLoginSubmit}>
+                            <label htmlFor="email">Email Address <span>*</span></label>
+                            <input onChange={handleOnChange} type="email" name="email" id="email" placeholder="Enter your email" required />
+
+                            <label htmlFor="password">Password <span>*</span></label>
+                            <input onChange={handleOnChange} type="password" name="password" id="password" placeholder="Enter your password" required />
+
+                            <div className="d-flex align-items-center justify-content-between">
+                                <Button type="submit" className="btn_regular">
+                                    Login
+                                </Button>
+                                <Link to="/register">
+                                    <Button className="btn_regular" variant="text">Register</Button>
+                                </Link>
+                            </div>
+
+                            <Button onClick={handleGoogleSignIn} className="btn_regular w-100 mt-4">
+                                Sign In With Google
+                            </Button>
+                        </form>
+                    </div>
+                </Col>
+            </Row>
         </div>
-        <div style={{ marginTop: "60px" }} className="col-lg-6 ">
-          <form onSubmit={handleLoginSubmit}>
-            <h3 className="py-4 text-style">Please Login</h3>
-            <label htmlFor="exampleInputEmail1"></label>
-            <input onChange={handleOnChange} className="w-75  mb-3" type="email" name="email" id="" placeholder="email" required />
-            <br />
-            <label htmlFor="exampleInputPassword1"></label>
-            <input onChange={handleOnChange} className="w-75 mb-3" type="password" name="password" id="" placeholder="password" required />
-            <br />
-            <Button type="submit" className="w-75  buynow-btn mb-3">
-              Login
-            </Button>
-            <br />
-            <Link to="/register">
-              <Button variant="text">New User? Please Register</Button>
-            </Link>
-            <br />
-            <Button onClick={handleGoogleSignIn} className="w-75 buynow-btn mb-3">
-              Google Sign In
-            </Button>
-          </form>
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Login;
